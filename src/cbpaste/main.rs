db8ca450f82fa1033fffee14e75f0e53d38c538b
Brad Lugo <brad@bradlugo.com>
2019-05-16 16:20:30 -0500

    Change simple implementation to use stdin and stdout

--- src/cbpaste/main.rs
+++ src/cbpaste/main.rs
@@ -2,8 +2,14 @@ extern crate clipboard;
 
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
+use std::io::{self, Write};
 
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    println!("{:?}", ctx.get_contents());
+    io::stdout().flush().unwrap();
+    match ctx.get_contents() {
+        Ok(text) => print!("{:?}", text),
+        Err(err) => print!("{:?}", err), // FIXME :: can't print err when expecting clipboard
+    }
+    io::stdout().flush().unwrap();
 }
